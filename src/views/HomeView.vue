6ab9891225f42f3da0a0c9f1c2070d9621e3ed3b
izorwebid <izorvmp@gmail.com>
2023-01-29 23:57:41 +0700

    add configuration color

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
     <p class="text-3xl text-gray-700 font-heading mb-5">Welcome!</p>
-    <p class="text-3xl text-gray-700 font-brandon font-light mb-5">Welcome!</p>
-    <p class="text-3xl text-gray-700 font-brandon font-normal mb-5">Welcome!</p>
-    <p class="text-3xl text-gray-700 font-brandon font-medium mb-5">Welcome!</p>
-    <p class="text-3xl text-gray-700 font-brandon font-bold mb-5">Welcome!</p>
+    <p
+      class="text-3xl text-gray-700 font-brandon text-flamingo font-light mb-5"
+    >
+      Welcome!
+    </p>
+    <p
+      class="text-3xl text-gray-700 font-brandon text-semantic-red font-normal mb-5"
+    >
+      Welcome!
+    </p>
+    <p
+      class="text-3xl text-gray-700 font-brandon text-semantic-green font-medium mb-5"
+    >
+      Welcome!
+    </p>
+    <p
+      class="text-3xl text-gray-700 font-brandon text-semantic-blue font-bold mb-5"
+    >
+      Welcome!
+    </p>
     <p class="text-gray-500 text-lg font-lato italic">
       Vue and Tailwind CSS in action
     </p>
