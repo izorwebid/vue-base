dc0102c3525ed43566761b2c2fce109dfad29f0c
izorwebid <izorvmp@gmail.com>
2023-01-29 23:39:19 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <p class="text-3xl text-gray-700 font-heading mb-5">Welcome!</p>
+    <p class="text-3xl text-gray-700 font-brandon font-light mb-5">Welcome!</p>
+    <p class="text-3xl text-gray-700 font-brandon font-normal mb-5">Welcome!</p>
+    <p class="text-3xl text-gray-700 font-brandon font-medium mb-5">Welcome!</p>
+    <p class="text-3xl text-gray-700 font-brandon font-bold mb-5">Welcome!</p>
+    <p class="text-gray-500 text-lg font-lato italic">
+      Vue and Tailwind CSS in action
+    </p>
+    <p class="text-gray-500 text-lg font-lato font-light">
+      Vue and Tailwind CSS in action
+    </p>
+    <p class="text-gray-500 text-lg font-lato font-normal">
+      Vue and Tailwind CSS in action
+    </p>
+
+    <p class="text-gray-500 text-lg font-lato font-bold">
+      Vue and Tailwind CSS in action
+    </p>
+  </div>
+</template>
+
+<script setup></script>
